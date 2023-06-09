50caac94a58247035f10c96db086405c331b7f29
JoshuaXu7 <90508867+JoshuaXu7@users.noreply.github.com>
2023-06-09 15:32:08 +0800

    Add files via upload
    examples for cross-plane problem

---
+++ bin/examples/cross-plane/plot_data_1D.m
@@ -0,0 +1,46 @@
+clc;clear all;close all
+%%
+load 1D//1e-8/Tempcell.dat;
+load analytic_201806/T1e-8.dat;
+L=1e-8;
+scatter(Tempcell(:,1)/L,Tempcell(:,4),80,'r');
+hold on
+
+plot(T1e_8(:,1),T1e_8(:,2),'b','LineWidth',4);
+hold on
+
+load 1D/1e-7/Tempcell.dat;
+load analytic_201806/T1e-7.dat;
+L=1e-7;
+scatter(Tempcell(:,1)/L,Tempcell(:,4),80,'r');
+hold on
+
+plot(T1e_7(:,1),T1e_7(:,2),'b','LineWidth',4);
+hold on
+
+
+load 1D/1e-6/Tempcell.dat;
+load analytic_201806/T1e-6.dat;
+L=1e-6;
+scatter(Tempcell(:,1)/L,Tempcell(:,4),80,'r');
+hold on
+
+plot(T1e_6(:,1),T1e_6(:,2),'b','LineWidth',4);
+hold on
+
+box on
+
+legend('Analytical','Present','FontSize',24);
+legend boxoff
+
+xlabel('X*','FontSize',24);
+
+ylabel('Y*','FontSize',24);
+
+set(gca,'FontSize',24);
+
+text(0.1,0.9,'L=1000 nm','FontSize',24)
+
+text(0.3,0.7,'L=100 nm','FontSize',24)
+
+text(0.1,0.5,'L=10 nm','FontSize',24)
