1a131571f540d8006db8087fe51643b65f72374a
JoshuaXu7 <90508867+JoshuaXu7@users.noreply.github.com>
2023-07-10 19:13:11 +0800

    Add files via upload
    matlab script for data post-processing

---
+++ bin/examples/interface/verify_temperature_interface.m
@@ -0,0 +1,48 @@
+clc;clear all;close all
+
+%%
+% load results from literature Ran, et al. (https://doi.org/10.1016/j.jcp.2019.108920)
+load Tempcell_ref_20.dat; % load temperature results for L=20nm
+
+load Tempcell_ref_50.dat; % load temperature results for L=50nm
+
+load Tempcell_ref_220.dat; % load temperature results for L=220nm
+
+plot(Tempcell_ref_20(:,1)/20e-9,Tempcell_ref_20(:,4),'b','LineWidth',2);
+hold on
+plot(Tempcell_ref_50(:,1)/50e-9,Tempcell_ref_50(:,4),'b','LineWidth',2);
+hold on
+h1=plot(Tempcell_ref_220(:,1)/220e-9,Tempcell_ref_220(:,4),'b','LineWidth',2);
+hold on
+
+%%
+% compare temperature results from GiftBTE and literature Ran, et al.
+load 20nm/TempLattice.dat; % load temperature results calculated from GiftBTE for L=20nm
+scatter(TempLattice(:,2)/20e-9,TempLattice(:,4),80,'r'); % non-dimensional x position versus temperature
+hold on
+
+load 50nm/TempLattice.dat; % load temperature results calculated from GiftBTE for L=50nm
+scatter(TempLattice(:,2)/50e-9,TempLattice(:,4),80,'r'); % non-dimensional x position versus temperature
+hold on
+
+load 220nm/TempLattice.dat; % load temperature results calculated from GiftBTE for L=220nm
+h2=scatter(TempLattice(:,2)/220e-9,TempLattice(:,4),80,'r'); % non-dimensional x position versus temperature
+hold on
+
+%%
+legend([h1 h2],'Ran et al','GiftBTE','FontSize',24,'Location','Southwest');
+legend boxoff
+
+xlabel('X*','FontSize',24);
+
+ylabel('T*','FontSize',24);
+
+set(gca,'FontSize',24);
+
+text(0.5,0.9,'L=20 nm','FontSize',24)
+
+text(0.5,0.8,'L=50 nm','FontSize',24)
+
+text(0.5,0.6,'L=220 nm','FontSize',24)
+
+box on
